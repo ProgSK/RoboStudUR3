13fdf485508d8995911130b709f963a6b0fdd324
Dominic Manno <dommanno02@gmail.com>
2024-06-03 10:35:10 +1000

    updated ros integration

--- Perception/ros_integration.py
+++ Perception/ros_integration.py
@@ -66,8 +66,8 @@ def publisher():
     box_height = None #Variable storing object height
 
     centre_point = [300, 65] #Variable for Belt centre of RGB image 
-    centre_point_depth = [200, 50] #Variable for Belt centre of Depth image 
-    mm_per_pixel = 198/367 #Variable storing pixel size ratio from test measurement 
+    centre_point_depth = [220, 40] #Variable for Belt centre of Depth image 
+    mm_per_pixel = 300/355 #Variable storing pixel size ratio from test measurement 
 
 
     col_depth_offset = [0, 50]
@@ -92,8 +92,8 @@ def publisher():
         color_image = np.asanyarray(color_frame.get_data())
 
         #Crop image to just belt
-        belt = color_image[90:245, 5:625]
-        belt_depth = depth_image [150:250, 70:480]
+        belt = color_image[190:300, 5:600]
+        belt_depth = depth_image [220:290, 90:480]
         
         # 60 - 400 y
         # 10 - 600 x
@@ -113,7 +113,7 @@ def publisher():
         #Changing Depth iamge to greyscale 
         img_gray_depth = cv2.cvtColor(belt_depth_colour, cv2.COLOR_BGR2GRAY)
         img_gray_depth = cv2.GaussianBlur(img_gray_depth, (7, 7), 0)
-        _, threshold_depth = cv2.threshold(img_gray_depth, 160, 190, cv2.THRESH_BINARY)
+        _, threshold_depth = cv2.threshold(img_gray_depth, 40, 45, cv2.THRESH_BINARY)
 
         #Finding contours in depth image
         cnts_depth = cv2.findContours(threshold_depth, cv2.RETR_EXTERNAL,
@@ -185,7 +185,7 @@ def publisher():
         #Changing colour image to grey scale
         img_gray = cv2.cvtColor(belt, cv2.COLOR_BGR2GRAY)
         img_gray = cv2.GaussianBlur(img_gray, (7, 7), 0)
-        _, threshold = cv2.threshold(img_gray, 100, 300, cv2.THRESH_BINARY)
+        _, threshold = cv2.threshold(img_gray, 55, 70, cv2.THRESH_BINARY)
 
         #Finding Contours in colour image
         cnts = cv2.findContours(threshold, cv2.RETR_EXTERNAL,
@@ -298,7 +298,7 @@ def publisher():
 
             rospy.loginfo("Published Perception Data Array: %s", percep_data)
 
-            if centx > 570:
+            if centx > 400:
                 box_height = None
 
             #use the box centre as a radius from the frame centre like below
